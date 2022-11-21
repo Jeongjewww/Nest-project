f8928b0333517d6a5d9ee753c265de8ebd007ac3
Jeongjewww <gwmelody99@gmail.com>
2022-11-21 14:55:25 +0900

    #2.8 typeorm module 연결 & Get api 수정

--- src/modeset/controllers/session.controller.ts
+++ src/modeset/controllers/session.controller.ts
@@ -20,32 +20,34 @@ export class SessionController {
   constructor(private readonly sessionServer: SessionService) {}
 
   @Get('/')
-  getAllSession(): Session[] {
-    return this.sessionServer.getAll();
+  async getAllSession(): Promise<Session[]> {
+    return await this.sessionServer.getAll();
   }
 
   @Get('/:id')
-  getOneSession(@Param('id', ParseIntPipe) sessionId: number): Session {
-    return this.sessionServer.getOne(sessionId);
+  async getOneSession(
+    @Param('id', ParseIntPipe) sessionId: number,
+  ): Promise<Session> {
+    return await this.sessionServer.getOne(sessionId);
   }
 
   // 전체 데이터 추가
-  @Post()
-  @UsePipes(ValidationPipe)
-  createSession(@Body(ValidationPipe) sessionData: CreateSessionDto) {
-    return this.sessionServer.create(sessionData);
-  }
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // createSession(@Body(ValidationPipe) sessionData: CreateSessionDto) {
+  //   return this.sessionServer.create(sessionData);
+  // }
 
-  @Delete('/:id')
-  deleteSession(@Param('id') sessionId: number): void {
-    return this.sessionServer.delete(sessionId);
-  }
+  // @Delete('/:id')
+  // deleteSession(@Param('id') sessionId: number): void {
+  //   return this.sessionServer.delete(sessionId);
+  // }
 
-  @Patch('/:id')
-  updateSession(
-    @Param('id') sessionId: number,
-    @Body() updateData: UpdateSessionDto,
-  ) {
-    return this.sessionServer.update(sessionId, updateData);
-  }
+  // @Patch('/:id')
+  // updateSession(
+  //   @Param('id') sessionId: number,
+  //   @Body() updateData: UpdateSessionDto,
+  // ) {
+  //   return this.sessionServer.update(sessionId, updateData);
+  // }
 }
--- src/modeset/services/session.service.ts
+++ src/modeset/services/session.service.ts
@@ -1,45 +1,50 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Session } from 'src/modeset/entities/session.entity';
+import { Repository } from 'typeorm';
 import { CreateSessionDto } from '../dto/create-session.dto';
 import { UpdateSessionDto } from '../dto/update-session.dto';
-import { SessionRepository } from '../repositories/session.repository';
 
 @Injectable()
 export class SessionService {
-  private server: Session[] = [];
+  constructor(
+    @InjectRepository(Session)
+    private sessionRepository: Repository<Session>,
+  ) {}
 
-  getAll(): Session[] {
-    return this.server;
+  // private server: Session[] = [];
+
+  getAll(): Promise<Session[]> {
+    return this.sessionRepository.find();
   }
 
-  getOne(id: number): Session {
-    const session = this.server.find((session) => session.id === id);
+  async getOne(id: number): Promise<Session> {
+    const session = await this.sessionRepository.findOneBy({ id });
     if (!session) {
       throw new NotFoundException(`${id}번 세션서버 정보를 찾을 수 없습니다.`);
     }
     return session;
   }
 
-  create(sessionData: CreateSessionDto) {
-    this.server.push({
-      id: this.server.length + 1,
-      ...sessionData,
-    });
-  }
+  // create(sessionData: CreateSessionDto) {
+  //   this.server.push({
+  //     id: this.server.length + 1,
+  //     ...sessionData,
+  //   });
+  // }
 
-  delete(id: number) {
-    const deleteData = this.getOne(id);
-    if (!deleteData) {
-      throw new NotFoundException(`${id}번 세션서버 정보를 찾을 수 없습니다.`);
-    } else {
-      this.server = this.server.filter((session) => session.id !== id);
-    }
-  }
+  // delete(id: number) {
+  //   const deleteData = this.getOne(id);
+  //   if (!deleteData) {
+  //     throw new NotFoundException(`${id}번 세션서버 정보를 찾을 수 없습니다.`);
+  //   } else {
+  //     this.server = this.server.filter((session) => session.id !== id);
+  //   }
+  // }
 
-  update(id: number, updateData: UpdateSessionDto) {
-    const oneSession = this.getOne(id);
-    this.delete(id);
-    this.server.push({ ...oneSession, ...updateData });
-  }
+  // update(id: number, updateData: UpdateSessionDto) {
+  //   const oneSession = this.getOne(id);
+  //   this.delete(id);
+  //   this.server.push({ ...oneSession, ...updateData });
+  // }
 }
